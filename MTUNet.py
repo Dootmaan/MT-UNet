1823e287a4380995cb754e14ab04be64aa090387
Wang.Hy <njdswhy@gmail.com>
2021-11-19 19:55:16 +0800

    file formatted

--- MTUNet.py
+++ MTUNet.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# author: zerone_xie
-# datetime:2021/8/7 13:31
-# software: PyCharm
-import math
 
+import math
 import numpy as np
 import torch
 from torch import nn
 
-
 class ConvBNReLU(nn.Module):
-    def __init__(self, c_in, c_out, kernel_size, stride=1, padding=1, activation=True):
+    def __init__(self,
+                 c_in,
+                 c_out,
+                 kernel_size,
+                 stride=1,
+                 padding=1,
+                 activation=True):
         super(ConvBNReLU, self).__init__()
-        self.conv = nn.Conv2d(
-            c_in, c_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=False
-        )
+        self.conv = nn.Conv2d(c_in,
+                              c_out,
+                              kernel_size=kernel_size,
+                              stride=stride,
+                              padding=padding,
+                              bias=False)
         self.bn = nn.BatchNorm2d(c_out)
         self.relu = nn.ReLU()
         self.activation = activation
@@ -29,13 +34,11 @@ class ConvBNReLU(nn.Module):
 
 
 class DoubleConv(nn.Module):
-
     def __init__(self, cin, cout):
         super(DoubleConv, self).__init__()
         self.conv = nn.Sequential(
             ConvBNReLU(cin, cout, 3, 1, padding=1),
-            ConvBNReLU(cout, cout, 3, stride=1, padding=1, activation=False)
-        )
+            ConvBNReLU(cout, cout, 3, stride=1, padding=1, activation=False))
         self.conv1 = nn.Conv2d(cout, cout, 1)
         self.relu = nn.ReLU()
         self.bn = nn.BatchNorm2d(cout)
@@ -51,7 +54,6 @@ class DoubleConv(nn.Module):
 
 
 class U_encoder(nn.Module):
-
     def __init__(self):
         super(U_encoder, self).__init__()
         self.res1 = DoubleConv(3, 64)
@@ -60,7 +62,6 @@ class U_encoder(nn.Module):
         self.pool2 = nn.MaxPool2d(2)
         self.res3 = DoubleConv(128, 256)
         self.pool3 = nn.MaxPool2d(2)
-
 
     def forward(self, x):
         features = []
@@ -80,7 +81,6 @@ class U_encoder(nn.Module):
 
 
 class U_decoder(nn.Module):
-
     def __init__(self):
         super(U_decoder, self).__init__()
         self.trans1 = nn.ConvTranspose2d(512, 256, 2, stride=2)
@@ -105,9 +105,6 @@ class U_decoder(nn.Module):
 
 
 class MEAttention(nn.Module):
-    '''
-    首先经过linear层计算query,然后分别计算k,v计算attention
-    '''
     def __init__(self, dim, configs):
         super(MEAttention, self).__init__()
         self.num_heads = configs["head"]
@@ -123,7 +120,8 @@ class MEAttention(nn.Module):
     def forward(self, x):
         B, N, C = x.shape
         x = self.query_liner(x)
-        x = x.view(B, N, self.num_heads, -1).permute(0, 2, 1, 3)  #（1， 32， 225， 32）
+        x = x.view(B, N, self.num_heads, -1).permute(0, 2, 1,
+                                                     3)  #（1， 32， 225， 32）
 
         attn = self.linear_0(x)
 
@@ -139,12 +137,6 @@ class MEAttention(nn.Module):
 
 class Attention(nn.Module):
     def __init__(self, dim, configs, axial=False):
-
-        ''' 每个patch有q,k,v
-        :param dim:
-        :param head:
-        '''
-
         super(Attention, self).__init__()
         self.axial = axial
         self.dim = dim
@@ -165,32 +157,31 @@ class Attention(nn.Module):
         return x
 
     def forward(self, x):
-        '''
-
-        :param x:size(b, h, w, c)
-        :return:
-        '''
-
         # first row and col attention
         if self.axial:
-
             # row attention (single head attention)
             b, h, w, c = x.shape
             mixed_query_layer = self.query_layer(x)
             mixed_key_layer = self.key_layer(x)
             mixed_value_layer = self.value_layer(x)
 
-            query_layer_x = mixed_query_layer.view(b*h, w, -1)
-            key_layer_x = mixed_key_layer.view(b*h, w, -1).transpose(-1, -2)
-            attention_scores_x = torch.matmul(query_layer_x, key_layer_x)  # (b*h, w, w, c)
-            attention_scores_x = attention_scores_x.view(b, -1, w, w)  # (b, h, w, w)
+            query_layer_x = mixed_query_layer.view(b * h, w, -1)
+            key_layer_x = mixed_key_layer.view(b * h, w, -1).transpose(-1, -2)
+            attention_scores_x = torch.matmul(query_layer_x,
+                                              key_layer_x)  # (b*h, w, w, c)
+            attention_scores_x = attention_scores_x.view(b, -1, w,
+                                                         w)  # (b, h, w, w)
 
             # col attention  (single head attention)
-
-            query_layer_y = mixed_query_layer.permute(0, 2, 1, 3).contiguous().view(b*w, h, -1)
-            key_layer_y = mixed_key_layer.permute(0, 2, 1, 3).contiguous().view(b*w, h, -1).transpose(-1, -2)
-            attention_scores_y = torch.matmul(query_layer_y, key_layer_y)  # (b*w, h, h, c)
-            attention_scores_y = attention_scores_y.view(b, -1, h, h)  # (b, w, h, h)
+            query_layer_y = mixed_query_layer.permute(0, 2, 1,
+                                                      3).contiguous().view(
+                                                          b * w, h, -1)
+            key_layer_y = mixed_key_layer.permute(
+                0, 2, 1, 3).contiguous().view(b * w, h, -1).transpose(-1, -2)
+            attention_scores_y = torch.matmul(query_layer_y,
+                                              key_layer_y)  # (b*w, h, h, c)
+            attention_scores_y = attention_scores_y.view(b, -1, h,
+                                                         h)  # (b, w, h, h)
 
             return attention_scores_x, attention_scores_y, mixed_value_layer
 
@@ -200,17 +191,25 @@ class Attention(nn.Module):
             mixed_key_layer = self.key_layer(x)
             mixed_value_layer = self.value_layer(x)
 
-            query_layer = self.transpose_for_scores(mixed_query_layer).permute(0, 1, 2, 4, 3, 5).contiguous()  # (b, p, p, head, n, c)
-            key_layer = self.transpose_for_scores(mixed_key_layer).permute(0, 1, 2, 4, 3, 5).contiguous()
-            value_layer = self.transpose_for_scores(mixed_value_layer).permute(0, 1, 2, 4, 3, 5).contiguous()
+            query_layer = self.transpose_for_scores(mixed_query_layer).permute(
+                0, 1, 2, 4, 3, 5).contiguous()  # (b, p, p, head, n, c)
+            key_layer = self.transpose_for_scores(mixed_key_layer).permute(
+                0, 1, 2, 4, 3, 5).contiguous()
+            value_layer = self.transpose_for_scores(mixed_value_layer).permute(
+                0, 1, 2, 4, 3, 5).contiguous()
 
-            attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-            attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+            attention_scores = torch.matmul(query_layer,
+                                            key_layer.transpose(-1, -2))
+            attention_scores = attention_scores / math.sqrt(
+                self.attention_head_size)
             atten_probs = self.softmax(attention_scores)
 
-            context_layer = torch.matmul(atten_probs, value_layer)  # (b, p, p, head, win, h)
-            context_layer = context_layer.permute(0, 1, 2, 4, 3, 5).contiguous()
-            new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size, )
+            context_layer = torch.matmul(
+                atten_probs, value_layer)  # (b, p, p, head, win, h)
+            context_layer = context_layer.permute(0, 1, 2, 4, 3,
+                                                  5).contiguous()
+            new_context_layer_shape = context_layer.size()[:-2] + (
+                self.all_head_size, )
             context_layer = context_layer.view(*new_context_layer_shape)
             attention_output = self.out(context_layer)
 
@@ -224,11 +223,6 @@ class WinAttention(nn.Module):
         self.attention = Attention(dim, configs)
 
     def forward(self, x):
-        '''
-
-        :param x: size(B, H*W, c)
-        :return:
-        '''
         b, n, c = x.shape
         h, w = int(np.sqrt(n)), int(np.sqrt(n))
         x = x.permute(0, 2, 1).contiguous().view(b, c, h, w)
@@ -236,39 +230,40 @@ class WinAttention(nn.Module):
             right_size = h + self.window_size - h % self.window_size
             new_x = torch.zeros((b, c, right_size, right_size))
             new_x[:, :, 0:x.shape[2], 0:x.shape[3]] = x[:]
-            new_x[:, :, x.shape[2]:, x.shape[3]:] = x[:, :, (x.shape[2]-right_size):, (x.shape[3]-right_size):]
+            new_x[:, :, x.shape[2]:,
+                  x.shape[3]:] = x[:, :, (x.shape[2] - right_size):,
+                                   (x.shape[3] - right_size):]
             x = new_x
             b, c, h, w = x.shape
-        x = x.view(b, c, h//self.window_size, self.window_size, w//self.window_size, self.window_size)
-        x = x.permute(0, 2, 4, 3, 5, 1).contiguous().view(b, h//self.window_size, w//self.window_size, self.window_size * self.window_size, c).cuda()
+        x = x.view(b, c, h // self.window_size, self.window_size,
+                   w // self.window_size, self.window_size)
+        x = x.permute(0, 2, 4, 3, 5,
+                      1).contiguous().view(b, h // self.window_size,
+                                           w // self.window_size,
+                                           self.window_size * self.window_size,
+                                           c).cuda()
         x = self.attention(x)  # (b, p, p, win, h)
         return x
 
 
 class DlightConv(nn.Module):
-
     def __init__(self, dim, configs):
         super(DlightConv, self).__init__()
-        self.linear = nn.Linear(dim, configs["win_size"]*configs["win_size"])
+        self.linear = nn.Linear(dim, configs["win_size"] * configs["win_size"])
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
-        '''
-
-        :param x: size(b, n, n, win, h)
-        :return:
-        '''
         h = x
         avg_x = torch.mean(x, dim=-2)  # (b, n, n, 1, h)
         x_prob = self.softmax(self.linear(avg_x))  # （b, n, n, win)
 
-        x = torch.mul(h, x_prob.unsqueeze(-1))    # (b, p, p, 16, h) (b, p, p, 16)
+        x = torch.mul(h,
+                      x_prob.unsqueeze(-1))  # (b, p, p, 16, h) (b, p, p, 16)
         x = torch.sum(x, dim=-2)  # （b, n, n, 1, h)
         return x
 
 
 class GaussianTrans(nn.Module):
-
     def __init__(self):
         super(GaussianTrans, self).__init__()
         self.bias = nn.Parameter(-torch.abs(torch.randn(1)))
@@ -284,8 +279,10 @@ class GaussianTrans(nn.Module):
                 atten_x = atten_x_full[:, r, c, :]  # (b, w)
                 atten_y = atten_y_full[:, c, r, :]  # (b, h)
 
-                dis_x = torch.tensor([(h-c)**2 for h in range(x.shape[2])]).cuda()  # (b, w)
-                dis_y = torch.tensor([(w-r)**2 for w in range(x.shape[1])]).cuda()  # (b, h)
+                dis_x = torch.tensor([(h - c)**2 for h in range(x.shape[2])
+                                      ]).cuda()  # (b, w)
+                dis_y = torch.tensor([(w - r)**2 for w in range(x.shape[1])
+                                      ]).cuda()  # (b, h)
 
                 dis_x = -(self.shift * dis_x + self.bias).cuda()
                 dis_y = -(self.shift * dis_y + self.bias).cuda()
@@ -293,18 +290,15 @@ class GaussianTrans(nn.Module):
                 atten_x = self.softmax(dis_x + atten_x)
                 atten_y = self.softmax(dis_y + atten_y)
 
-                new_value_full[:, r, c, :] = torch.sum(atten_x.unsqueeze(dim=-1) * value_full[:, r, :, :] + atten_y.unsqueeze(dim=-1) * value_full[:, :, c, :], dim=-2)
+                new_value_full[:, r, c, :] = torch.sum(
+                    atten_x.unsqueeze(dim=-1) * value_full[:, r, :, :] +
+                    atten_y.unsqueeze(dim=-1) * value_full[:, :, c, :],
+                    dim=-2)
         return new_value_full
 
 
 class CSAttention(nn.Module):
-
     def __init__(self, dim, configs):
-        '''
-
-        :param dim:
-        :param axis: h,w的数量
-        '''
         super(CSAttention, self).__init__()
         self.win_atten = WinAttention(configs, dim)
         self.dlightconv = DlightConv(dim, configs)
@@ -313,7 +307,7 @@ class CSAttention(nn.Module):
         #self.conv = nn.Conv2d(dim, dim, 3, padding=1)
         #self.maxpool = nn.MaxPool2d(2)
         self.up = nn.UpsamplingBilinear2d(scale_factor=4)
-        self.queeze = nn.Conv2d(2*dim, dim, 1)
+        self.queeze = nn.Conv2d(2 * dim, dim, 1)
 
     def forward(self, x):
         '''
@@ -322,20 +316,25 @@ class CSAttention(nn.Module):
         :return:
         '''
         origin_size = x.shape
-        _, origin_h, origin_w, _ = origin_size[0], int(np.sqrt(origin_size[1])), int(np.sqrt(origin_size[1])), origin_size[2]
+        _, origin_h, origin_w, _ = origin_size[0], int(np.sqrt(
+            origin_size[1])), int(np.sqrt(origin_size[1])), origin_size[2]
         x = self.win_atten(x)  # (b, p, p, win, h)
         b, p, p, win, c = x.shape
-        h = x.view(b, p, p, int(np.sqrt(win)), int(np.sqrt(win)), c).permute(0, 1, 3, 2, 4, 5).contiguous()
-        h = h.view(b, p*int(np.sqrt(win)), p*int(np.sqrt(win)), c).permute(0, 3, 1, 2).contiguous()  # (b, c, h, w)
+        h = x.view(b, p, p, int(np.sqrt(win)), int(np.sqrt(win)),
+                   c).permute(0, 1, 3, 2, 4, 5).contiguous()
+        h = h.view(b, p * int(np.sqrt(win)), p * int(np.sqrt(win)),
+                   c).permute(0, 3, 1, 2).contiguous()  # (b, c, h, w)
 
         x = self.dlightconv(x)  # (b, n, n, h)
-        atten_x, atten_y, mixed_value = self.global_atten(x)  # (atten_x, atten_y, value)
+        atten_x, atten_y, mixed_value = self.global_atten(
+            x)  # (atten_x, atten_y, value)
         gaussian_input = (x, atten_x, atten_y, mixed_value)
-        x = self.gaussiantrans(gaussian_input)   # (b, h, w, c)
+        x = self.gaussiantrans(gaussian_input)  # (b, h, w, c)
         x = x.permute(0, 3, 1, 2).contiguous()
 
         x = self.up(x)
-        x = self.queeze(torch.cat((x, h), dim=1)).permute(0, 2, 3, 1).contiguous()
+        x = self.queeze(torch.cat((x, h), dim=1)).permute(0, 2, 3,
+                                                          1).contiguous()
         x = x[:, :origin_h, :origin_w, :].contiguous()
         x = x.view(b, -1, c)
 
@@ -343,7 +342,6 @@ class CSAttention(nn.Module):
 
 
 class EAmodule(nn.Module):
-
     def __init__(self, dim):
         super(EAmodule, self).__init__()
         self.SlayerNorm = nn.LayerNorm(dim, eps=1e-6)
@@ -368,7 +366,6 @@ class EAmodule(nn.Module):
 
 
 class DecoderStem(nn.Module):
-
     def __init__(self):
         super(DecoderStem, self).__init__()
         self.block = U_decoder()
@@ -379,25 +376,24 @@ class DecoderStem(nn.Module):
 
 
 class Stem(nn.Module):
-
     def __init__(self):
         super(Stem, self).__init__()
         self.model = U_encoder()
-        self.trans_dim = ConvBNReLU(256, 256, 1, 1, 0) #out_dim, model_dim
-        self.position_embedding = nn.Parameter(torch.zeros((1, 784, 256)))  #缺少n_patches, hiddensize参数
+        self.trans_dim = ConvBNReLU(256, 256, 1, 1, 0)  #out_dim, model_dim
+        self.position_embedding = nn.Parameter(torch.zeros(
+            (1, 784, 256)))  #缺少n_patches, hiddensize参数
 
     def forward(self, x):
 
         x, features = self.model(x)  # (1, 512, 28, 28)
         x = self.trans_dim(x)  # (B, C, H, W) (1, 256, 28, 28)
-        x = x.flatten(2)    # (B, H, N)  (1, 256, 28*28)
+        x = x.flatten(2)  # (B, H, N)  (1, 256, 28*28)
         x = x.transpose(-2, -1)  #  (B, N, H)
         x = x + self.position_embedding
-        return x, features      #(B, N, H)
+        return x, features  #(B, N, H)
 
 
 class encoder_block(nn.Module):
-
     def __init__(self, dim):
         super(encoder_block, self).__init__()
         self.block = nn.ModuleList([
@@ -411,27 +407,35 @@ class encoder_block(nn.Module):
         x = self.block[1](x)
         B, N, C = x.shape
         h, w = int(np.sqrt(N)), int(np.sqrt(N))
-        x = x.view(B, h, w, C).permute(0, 3, 1, 2)  # (1, 256, 28, 28) B, C, H, W
+        x = x.view(B, h, w, C).permute(0, 3, 1,
+                                       2)  # (1, 256, 28, 28) B, C, H, W
         skip = x
-        x = self.block[2](x)   # (14, 14, 256)
+        x = self.block[2](x)  # (14, 14, 256)
         return x, skip
 
 
 class decoder_block(nn.Module):
-
     def __init__(self, dim, flag):
         super(decoder_block, self).__init__()
         self.flag = flag
         if not self.flag:
             self.block = nn.ModuleList([
-                nn.ConvTranspose2d(dim, dim // 2, kernel_size=2, stride=2, padding=0),
+                nn.ConvTranspose2d(dim,
+                                   dim // 2,
+                                   kernel_size=2,
+                                   stride=2,
+                                   padding=0),
                 nn.Conv2d(dim, dim // 2, kernel_size=1, stride=1),
                 EAmodule(dim // 2),
                 EAmodule(dim // 2)
             ])
         else:
             self.block = nn.ModuleList([
-                nn.ConvTranspose2d(dim, dim // 2, kernel_size=2, stride=2, padding=0),
+                nn.ConvTranspose2d(dim,
+                                   dim // 2,
+                                   kernel_size=2,
+                                   stride=2,
+                                   padding=0),
                 EAmodule(dim),
                 EAmodule(dim)
             ])
@@ -458,22 +462,19 @@ class decoder_block(nn.Module):
 
 
 class MTUNet(nn.Module):
-
-    def __init__(self,out_ch=4):
+    def __init__(self, out_ch=4):
         super(MTUNet, self).__init__()
         self.stem = Stem()
         self.encoder = nn.ModuleList()
-        self.bottleneck = nn.Sequential(
-            EAmodule(configs["bottleneck"]),
-            EAmodule(configs["bottleneck"])
-        )
+        self.bottleneck = nn.Sequential(EAmodule(configs["bottleneck"]),
+                                        EAmodule(configs["bottleneck"]))
         self.decoder = nn.ModuleList()
 
         self.decoder_stem = DecoderStem()
         for i in range(len(configs["encoder"])):
             dim = configs["encoder"][i]
             self.encoder.append(encoder_block(dim))
-        for i in range(len(configs["decoder"])-1):
+        for i in range(len(configs["decoder"]) - 1):
             dim = configs["decoder"][i]
             self.decoder.append(decoder_block(dim, False))
         self.decoder.append(decoder_block(configs["decoder"][-1], True))
@@ -487,15 +488,17 @@ class MTUNet(nn.Module):
         for i in range(len(self.encoder)):
             x, skip = self.encoder[i](x)
             skips.append(skip)
-            B, C, H, W = x.shape    #  (1, 512, 8, 8)
+            B, C, H, W = x.shape  #  (1, 512, 8, 8)
             x = x.permute(0, 2, 3, 1).contiguous().view(B, -1, C)  # （B, N, C)
-        x = self.bottleneck(x)   # (1, 25, 1024)
+        x = self.bottleneck(x)  # (1, 25, 1024)
         B, N, C = x.shape
         x = x.view(B, int(np.sqrt(N)), -1, C).permute(0, 3, 1, 2)
         for i in range(len(self.decoder)):
-            x = self.decoder[i](x, skips[len(self.decoder) - i - 1])  # (B， N, C）
+            x = self.decoder[i](x,
+                                skips[len(self.decoder) - i - 1])  # (B， N, C）
             B, N, C = x.shape
-            x = x.view(B, int(np.sqrt(N)), int(np.sqrt(N)), C).permute(0, 3, 1, 2)
+            x = x.view(B, int(np.sqrt(N)), int(np.sqrt(N)),
+                       C).permute(0, 3, 1, 2)
 
         x = self.decoder_stem(x, features)
         x = self.SegmentationHead(x)
